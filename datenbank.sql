0614773de5cceee2e38ec72a4f03ee5f01bd7fc8
FineMaja <pepperli@web.de>
2023-05-08 14:58:12 +0200

    josefine: Tabellen erstellen

---
+++ datenbank.sql
@@ -0,0 +1,19 @@
+# Tabelle artist erstellen
+CREATE TABLE artist(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    name VARCHAR(100),
+    genre VARCHAR(30),
+    birth YEAR,
+    death YEAR, 
+    nationality VARCHAR (30)
+) COMMENT '';
+
+# Tabelle painitings erstellen
+CREATE TABLE paintings(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    artist_ID INT
+) COMMENT '';
+
+# Fremdschlüssel vergeben
+Alter Table paintings
+ADD FOREIGN KEY (artist_ID) REFERENCES artist (id);

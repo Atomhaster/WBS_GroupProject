40df761353806f7a6d8f2896c9215d6dc3c22e94
Burkhard von Dewitz <burkhard.dewitz@gmail.com>
2023-05-22 11:14:52 +0200

    added magic method __eq__ to artist module

--- modules/artist.py
+++ modules/artist.py
@@ -8,11 +8,13 @@ class artist():
     
     def delete_artist(self, id_artist):
         statement = f"DELETE * FROM artist Where id_artist={id_artist}"
-    
-        
+
     def insert_data(self,a,b,c,d,e):
         query= f"""Insert into artist (name, genre, birth, death, nationality)
         VALUES ('{a}','{b}', '{c}', '{d}','{e}');"""
         
-        
+    def __eq__(self, __o: object) -> bool:
+        if self.name == __o.name:
+            return True
+        else: return False
         

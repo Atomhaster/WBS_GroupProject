7bfe818c0e30c2e099f6594534077d33be72e8c0
Burkhard von Dewitz <burkhard.dewitz@gmail.com>
2023-05-23 08:52:41 +0200

    constructor of class painting is functional for initializing the painting

--- modules/painting.py
+++ modules/painting.py
@@ -1,39 +1,21 @@
 import db_remote as db
-import random
 
 class painting():
     
-    def __init__(self, id=0, artist_name="", artist_id=0) -> None:
+    def __init__(self, id=0, **args) -> None:
         gallery = db.db_azure()
         db_entry = ()
         if id:
-            db_entry = (gallery.get_painting(id))
-        elif artist_name:
-            art_id = gallery.get_artist_id(artist_name)
-            print(art_id)
-        elif artist_id:
-            print("art.id given")
+            db_entry = gallery.get_painting(id)
         else:
-            print("random picture")
+            db_entry = gallery.get_random_painting(**args)
             
-        # self.id = db_entry[0]
-        # self.artist_id = db_entry[1]
-        # self.size_width = db_entry[2]
-        # self.size_height = db_entry[2]
-        # self.ndarray = db_entry[4]
-        
+        self.id = db_entry[0]
+        self.artist_id = db_entry[1]
+        self.size_width = db_entry[2]
+        self.size_height = db_entry[2]
+        self.ndarray = db_entry[4]
     
-    def get_pic(self, id_pic):
-        statement = f"SELECT * FROM painting Where {id_pic}"
-        self.curser.execute(statement)
-    
-    def get_artist(self, id_art):
-        statement = f"SELECT * FROM artist Where {id_art}"
-        self.curser.execute(statement)
-    
-    def delete_Artist(self):
-        # delted artist from db
-        pass
     def blurr_image(self):
         # blurring the image
         pass
@@ -50,3 +32,7 @@ class painting():
 if __name__ == "__main__":
     test_painting = painting(artist_name="Marc Chagall")
     print(test_painting.artist_id)
+    
+    import matplotlib.pyplot as plt
+    imgplot = plt.imshow(test_painting.ndarray)
+    plt.show()
